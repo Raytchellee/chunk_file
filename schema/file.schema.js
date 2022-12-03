8a74c1072ade7f96c88b7b7cc4d403e7db613626
raytchellee <amakaoforkaja@gmail.com>
2022-12-03 22:28:13 +0100

    initialized repository

---
+++ schema/file.schema.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const fileSchema = new mongoose.Schema({
+  fileName: String,
+  fileExtension: String,
+  chunks: [Buffer],
+});
+
+module.exports = fileSchema;
