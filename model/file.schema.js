07951cecfd168bd37bae23937c42e5bc30e445d6
raytchellee <amakaoforkaja@gmail.com>
2022-12-14 00:10:50 +0100

    modified and added new files to the repo

--- model/file.schema.js
+++ model/file.schema.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+
 const fileSchema = new mongoose.Schema({
   fileName: String,
   fileExtension: String,
--- model/user.schema.js
+++ model/user.schema.js
@@ -1,10 +1,44 @@
 const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
 const fileSchema = require("./file.schema");
+const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  files: [fileSchema],
+const userSchema = new Schema(
+  {
+    // username: {
+    //   type: String,
+    //   required: true,
+    //   unique: true,
+    // },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    files: [fileSchema],
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+  const hash = await bcrypt.hash(this.password, 10);
+
+  this.password = hash;
+  next();
 });
 
-module.exports = userSchema;
+userSchema.methods.isValidPassword = async function (password) {
+  const user = this;
+  const compare = await bcrypt.compare(password, user.password);
+
+  return compare;
+};
+
+const userModel = model("userModel", userSchema);
+module.exports = userModel;
+// module.exports = userSchema;
