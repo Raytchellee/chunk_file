c0ac7a7557ce48192c99d7c58cebe36d2eec0e5a
raytchellee <amakaoforkaja@gmail.com>
2022-12-14 00:10:08 +0100

    modified and added new files to the repo

--- app.js
+++ app.js
@@ -4,8 +4,22 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-const indexRouter = require("./controllers/index");
-const usersRouter = require("./controllers/users");
+// const indexRouter = require("./controllers/index");
+// const usersRouter = require("./controllers/users");
+
+require("./services/auth.service");
+
+const routes = require("./routes/routes");
+const secureRoute = require("./routes/secure-routes");
+
+// const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+// const userModel = require("./model/user.schema");
+
+const connect = require("./utils/database");
+connect();
 
 const app = express();
 
@@ -18,8 +32,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// app.use("/", indexRouter);
+// app.use("/users", usersRouter);
+
+app.use("/", routes);
+
+// Plug in the JWT strategy as a middleware so only verified users can access this route.
+app.use("/user", passport.authenticate("jwt", { session: false }), secureRoute);
+
+// Handle errors.
+// app.use(function (err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.json({ error: err });
+// });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -36,5 +61,9 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
+
+// app.listen(3000, () => {
+//   console.log("Server started.");
+// });
 
 module.exports = app;
