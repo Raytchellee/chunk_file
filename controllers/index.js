6fcf49a7dfdb8a285dad7e30cff6b189187382f9
raytchellee <amakaoforkaja@gmail.com>
2022-12-14 00:31:10 +0100

    modified controllers/index.js and routes/routes.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,49 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// var router = express.Router();
 
-module.exports = router;
+// /* GET home page. */
+// router.get("/", function (req, res, next) {
+//   res.render("index", { title: "Express" });
+// });
+exports.homepage = async (req, res, next) => {
+  res.json({
+    message: "Reached home page succesfully",
+    // user: req.user,
+    user: "You",
+  });
+};
+
+exports.signup = async (req, res, next) => {
+  res.json({
+    message: "Signup successful",
+    user: req.user,
+  });
+};
+
+exports.login = async (req, res, next) => {
+  passport.authenticate("login", async (err, user, info) => {
+    try {
+      if (err || !user) {
+        const error = new Error("An error occurred.");
+
+        return next(error);
+      }
+
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error);
+
+        const body = { _id: user._id, email: user.email };
+        const token = jwt.sign({ user: body }, "TOP_SECRET");
+
+        return res.json({ token });
+      });
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
+};
+
+// module.exports = router;
